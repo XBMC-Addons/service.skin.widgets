b3adf52919c0e0258e50ff3164fa2231c6fe173c
Martijn Kaijser <mcm.kaijser@gmail.com>
2013-12-21 19:52:53 +0100

    move streamdetails and path to utils lib

--- default.py
+++ default.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#     Copyright (C) 2012 Team-XBMC
+#     Copyright (C) 2012-2013 Team-XBMC
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    the Free Software Foundation, either version 2 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -30,6 +30,7 @@ import _strptime
 import urllib
 import lib.common
 from lib.common import log
+from lib.utils import media_path, media_streamdetails
 
 if sys.version_info < (2, 7):
     import simplejson
@@ -653,77 +654,6 @@ class Main:
                 self._fetch_song('RandomSong')
                 self._fetch_addon('RandomAddon')
 
-def media_path(path):
-    # Check for stacked movies
-    try:
-        path = os.path.split(path)[0].rsplit(' , ', 1)[1].replace(",,",",")
-    except:
-        path = os.path.split(path)[0]
-    # Fixes problems with rared movies and multipath
-    if path.startswith("rar://"):
-        path = [os.path.split(urllib.url2pathname(path.replace("rar://","")))[0]]
-    elif path.startswith("multipath://"):
-        temp_path = path.replace("multipath://","").split('%2f/')
-        path = []
-        for item in temp_path:
-            path.append(urllib.url2pathname(item))
-    else:
-        path = [path]
-    return path[0]
-
-def media_streamdetails(filename, streamdetails):
-    info = {}
-    video = streamdetails['video']
-    audio = streamdetails['audio']
-    if '3d' in filename:
-        info['videoresolution'] = '3d'
-    elif video:
-        videowidth = video[0]['width']
-        videoheight = video[0]['height']
-        if (video[0]['width'] <= 720 and video[0]['height'] <= 480):
-            info['videoresolution'] = "480"
-        elif (video[0]['width'] <= 768 and video[0]['height'] <= 576):
-            info['videoresolution'] = "576"
-        elif (video[0]['width'] <= 960 and video[0]['height'] <= 544):
-            info['videoresolution'] = "540"
-        elif (video[0]['width'] <= 1280 and video[0]['height'] <= 720):
-            info['videoresolution'] = "720"
-        elif (video[0]['width'] >= 1281 or video[0]['height'] >= 721):
-            info['videoresolution'] = "1080"
-        else:
-            info['videoresolution'] = ""
-    elif (('dvd') in filename and not ('hddvd' or 'hd-dvd') in filename) or (filename.endswith('.vob' or '.ifo')):
-        info['videoresolution'] = '576'
-    elif (('bluray' or 'blu-ray' or 'brrip' or 'bdrip' or 'hddvd' or 'hd-dvd') in filename):
-        info['videoresolution'] = '1080'
-    else:
-        info['videoresolution'] = '1080'
-    if video:
-        info['videocodec'] = video[0]['codec']
-        if (video[0]['aspect'] < 1.4859):
-            info['videoaspect'] = "1.33"
-        elif (video[0]['aspect'] < 1.7190):
-            info['videoaspect'] = "1.66"
-        elif (video[0]['aspect'] < 1.8147):
-            info['videoaspect'] = "1.78"
-        elif (video[0]['aspect'] < 2.0174):
-            info['videoaspect'] = "1.85"
-        elif (video[0]['aspect'] < 2.2738):
-            info['videoaspect'] = "2.20"
-        else:
-            info['videoaspect'] = "2.35"
-    else:
-        info['videocodec'] = ''
-        info['videoaspect'] = ''
-    if audio:
-        info['audiocodec'] = audio[0]['codec']
-        info['audiochannels'] = audio[0]['channels']
-    else:
-        info['audiocodec'] = ''
-        info['audiochannels'] = ''
-    return info
-
-    
 class Widgets_Monitor(xbmc.Monitor):
     def __init__(self, *args, **kwargs):
         xbmc.Monitor.__init__(self)
---
+++ lib/utils.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#     Copyright (C) 2012-2013 Team-XBMC
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+
+def media_path(path):
+    # Check for stacked movies
+    try:
+        path = os.path.split(path)[0].rsplit(' , ', 1)[1].replace(",,",",")
+    except:
+        path = os.path.split(path)[0]
+    # Fixes problems with rared movies and multipath
+    if path.startswith("rar://"):
+        path = [os.path.split(urllib.url2pathname(path.replace("rar://","")))[0]]
+    elif path.startswith("multipath://"):
+        temp_path = path.replace("multipath://","").split('%2f/')
+        path = []
+        for item in temp_path:
+            path.append(urllib.url2pathname(item))
+    else:
+        path = [path]
+    return path[0]
+
+def media_streamdetails(filename, streamdetails):
+    info = {}
+    video = streamdetails['video']
+    audio = streamdetails['audio']
+    if '3d' in filename:
+        info['videoresolution'] = '3d'
+    elif video:
+        videowidth = video[0]['width']
+        videoheight = video[0]['height']
+        if (video[0]['width'] <= 720 and video[0]['height'] <= 480):
+            info['videoresolution'] = "480"
+        elif (video[0]['width'] <= 768 and video[0]['height'] <= 576):
+            info['videoresolution'] = "576"
+        elif (video[0]['width'] <= 960 and video[0]['height'] <= 544):
+            info['videoresolution'] = "540"
+        elif (video[0]['width'] <= 1280 and video[0]['height'] <= 720):
+            info['videoresolution'] = "720"
+        elif (video[0]['width'] >= 1281 or video[0]['height'] >= 721):
+            info['videoresolution'] = "1080"
+        else:
+            info['videoresolution'] = ""
+    elif (('dvd') in filename and not ('hddvd' or 'hd-dvd') in filename) or (filename.endswith('.vob' or '.ifo')):
+        info['videoresolution'] = '576'
+    elif (('bluray' or 'blu-ray' or 'brrip' or 'bdrip' or 'hddvd' or 'hd-dvd') in filename):
+        info['videoresolution'] = '1080'
+    else:
+        info['videoresolution'] = '1080'
+    if video:
+        info['videocodec'] = video[0]['codec']
+        if (video[0]['aspect'] < 1.4859):
+            info['videoaspect'] = "1.33"
+        elif (video[0]['aspect'] < 1.7190):
+            info['videoaspect'] = "1.66"
+        elif (video[0]['aspect'] < 1.8147):
+            info['videoaspect'] = "1.78"
+        elif (video[0]['aspect'] < 2.0174):
+            info['videoaspect'] = "1.85"
+        elif (video[0]['aspect'] < 2.2738):
+            info['videoaspect'] = "2.20"
+        else:
+            info['videoaspect'] = "2.35"
+    else:
+        info['videocodec'] = ''
+        info['videoaspect'] = ''
+    if audio:
+        info['audiocodec'] = audio[0]['codec']
+        info['audiochannels'] = audio[0]['channels']
+    else:
+        info['audiocodec'] = ''
+        info['audiochannels'] = ''
+    return info
